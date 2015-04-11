1492ac1144becb27f280758df846b294b4c0907e
lelabe <bardhiornela@yahoo.com>
2015-04-11 19:25:57 +0300

    buttons1

---
+++ core/src/com/spaceapps/liftoff/weather/Weather.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.spaceapps.liftoff.weather;
+import java.util.Random;
+
+/**
+ *
+ * @author Ornela
+ */
+public class Weather {
+    public enum weather {
+      Normal, Wind, Storm
+  }
+    public void update (){
+      if (weather==Normal)   
+    }
+    
+    
+  //weather={1,2,3};
+  
+    
+}
