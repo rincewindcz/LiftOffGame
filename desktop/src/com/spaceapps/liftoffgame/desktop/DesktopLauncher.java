ecd9f6ddca5256baa7b2321541741ee360bac911
Michal <michal.genserek@lut.fi>
2015-04-11 13:55:13 +0300

    fixed desktop launcher

--- desktop/src/com/spaceapps/liftoffgame/desktop/DesktopLauncher.java
+++ desktop/src/com/spaceapps/liftoffgame/desktop/DesktopLauncher.java
@@ -7,6 +7,6 @@ import com.spaceapps.liftoffgame.LiftOffGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new LiftOffGame(), config);
+		new LwjglApplication(LiftOffGame.getInstance(), config);
 	}
 }
