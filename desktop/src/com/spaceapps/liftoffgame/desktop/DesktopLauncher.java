be3a588c18090345adf849d812876bc72aee9d01
Michal <michal.genserek@lut.fi>
2015-04-11 11:50:31 +0300

    Initial commit

---
+++ desktop/src/com/spaceapps/liftoffgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.spaceapps.liftoffgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.spaceapps.liftoffgame.LiftOffGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LiftOffGame(), config);
+	}
+}
