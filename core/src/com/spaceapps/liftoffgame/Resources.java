41f77b42f8055216b0fe570f6f988e4c2947755d
rincewindcz <michal.genserek@lut.fi>
2015-04-11 19:54:51 +0300

    background

--- core/src/com/spaceapps/liftoffgame/Resources.java
+++ core/src/com/spaceapps/liftoffgame/Resources.java
@@ -9,6 +9,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
@@ -20,78 +21,82 @@ import java.util.HashMap;
  * @author craw
  */
 public class Resources {
+  
+  public final static String GAME_ATLAS = "atlas/atlas.atlas";
+  public final static String STANDARD_FONT = "font/ribeye.fnt";
+  public final static String MUSIC = "sound/music.ogg";
+  public final static String BACKGROUND = "background.png";
+  public final static float RATIO = 1.0f;
+  
+  public TextureAtlas atlas;
+  public AssetManager assetManager = new AssetManager();
+  public BitmapFont fontStandard;
+  private final HashMap<String, Sprite> sprites = new HashMap<String, Sprite>(64);
+  private Sprite emptySprite;
+  public Sound[] sounds;
+  public Music   music;
+  public Texture backgroundTexture;
 
-    public final static String GAME_ATLAS = "atlas/atlas.atlas";
-    public final static String STANDARD_FONT = "font/ribeye.fnt";
-    public final static String MUSIC = "sound/music.ogg";
-    public final static float RATIO = 1.0f;
+  public Resources() {
+    loadAssets();
+  }
 
-    public TextureAtlas atlas;
-    public AssetManager assetManager = new AssetManager();
-    public BitmapFont fontStandard;
-    private final HashMap<String, Sprite> sprites = new HashMap<String, Sprite>(64);
-    private Sprite emptySprite;
-    public Sound[] sounds;
-    public Music music;
-
-    public Resources() {
-        loadAssets();
+  public void playSound(int n) {
+      sounds[n].play();
+  }
+  
+  public void loadFont() {
+    fontStandard = new BitmapFont(Gdx.files.internal(STANDARD_FONT));
+  }
+  
+  public Sprite getSprite(String name) {
+    Sprite sprite = sprites.get(name);
+    return (sprite != null) ? sprite : emptySprite;
+  }
+  
+  public Sprite getNewSprite(String name) {
+    Sprite sprite = sprites.get(name);
+    return new Sprite((sprite != null) ? sprite : emptySprite);
+  }
+  
+  private void createSprites(float ratio) {
+    Array<TextureAtlas.AtlasRegion> regions = atlas.getRegions();
+    for (int i = 0; i < regions.size; i++) {
+      
+      Sprite sprite = new Sprite(regions.get(i));
+      
+      sprite.setSize(sprite.getWidth()*ratio, sprite.getHeight()*ratio);
+      sprite.setOrigin(sprite.getWidth()/2f, sprite.getHeight()/2f);
+      
+      sprites.put(regions.get(i).name, sprite);
+      
+      if (i == 0) {
+        emptySprite = sprite;
+      }
     }
-
-    public void playSound(int n) {
-        sounds[n].play();
+    LiftOffGame.logInfo(regions.size + " sprites loaded and created.");
+  }
+  
+  private void loadSounds(int number) {
+    sounds = new Sound[number];
+    for (int i =0; i < number; i++) {
+      sounds[i] = Gdx.audio.newSound(Gdx.files.internal("sound/sound" + i + ".wav"));
     }
+  }
+  
+  private void loadAssets() {
+    assetManager.load(GAME_ATLAS, TextureAtlas.class);
+    assetManager.load(BACKGROUND, Texture.class);
+    assetManager.finishLoading();
+    
+    atlas = assetManager.get(GAME_ATLAS);
+    backgroundTexture = assetManager.get(BACKGROUND);
+    music = Gdx.audio.newMusic(Gdx.files.internal(MUSIC));
 
-    public void loadFont() {
-        fontStandard = new BitmapFont(Gdx.files.internal(STANDARD_FONT));
-    }
-
-    public Sprite getSprite(String name) {
-        Sprite sprite = sprites.get(name);
-        return (sprite != null) ? sprite : emptySprite;
-    }
-
-    public Sprite getNewSprite(String name) {
-        Sprite sprite = sprites.get(name);
-        return new Sprite((sprite != null) ? sprite : emptySprite);
-    }
-
-    private void createSprites(float ratio) {
-        Array<TextureAtlas.AtlasRegion> regions = atlas.getRegions();
-        for (int i = 0; i < regions.size; i++) {
-            Sprite sprite = new Sprite(regions.get(i));
-
-            sprite.setSize(sprite.getWidth() * ratio, sprite.getHeight() * ratio);
-            sprite.setOrigin(sprite.getWidth() / 2f, sprite.getHeight() / 2f);
-
-            sprites.put(regions.get(i).name, sprite);
-
-            if (i == 0) {
-                emptySprite = sprite;
-            }
-        }
-        LiftOffGame.logInfo(regions.size + " sprites loaded and created.");
-    }
-
-    private void loadSounds(int number) {
-        sounds = new Sound[number];
-        for (int i = 0; i < number; i++) {
-            sounds[i] = Gdx.audio.newSound(Gdx.files.internal("sound/sound" + i + ".wav"));
-        }
-    }
-
-    private void loadAssets() {
-        assetManager.load(GAME_ATLAS, TextureAtlas.class);
-        //assetManager.load("ground.png", Texture.class);
-        assetManager.finishLoading();
-
-        atlas = assetManager.get(GAME_ATLAS);
-        //groundTexture = assetManager.get("ground.png");
-        music = Gdx.audio.newMusic(Gdx.files.internal(MUSIC));
-
-        createSprites(RATIO);
-
+    createSprites(RATIO);
+    
     //loadSounds(5);
-        LiftOffGame.logInfo("Loading assets done!");
-    }
+
+    LiftOffGame.logInfo("Loading assets done!");
+  }
 }
--- core/src/com/spaceapps/liftoffgame/screens/GameScreen.java
+++ core/src/com/spaceapps/liftoffgame/screens/GameScreen.java
@@ -8,6 +8,7 @@ package com.spaceapps.liftoffgame.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -26,6 +27,7 @@ public class GameScreen extends ScreenAdapter {
   private final Stage stage;
   private final SpriteBatch spriteBatch;
   private final Game game;
+  private final Texture backgroundTexture;
   
   private ButtonActor engineButton;
   private ButtonActor cargoButton;
@@ -39,6 +41,7 @@ public class GameScreen extends ScreenAdapter {
   public GameScreen() {
     spriteBatch = LiftOffGame.getInstance().getSpriteBatch();
     stage = new Stage(new StretchViewport(1024, 600), spriteBatch);
+    backgroundTexture = LiftOffGame.getInstance().resources.backgroundTexture;
 
     Gdx.input.setInputProcessor(stage);
     
@@ -139,13 +142,21 @@ public class GameScreen extends ScreenAdapter {
 
   @Override
   public void render(float delta) {
-    Gdx.gl.glClearColor(1, 1, 0, 1);
+    Gdx.gl.glClearColor(1, 1, 1, 1);
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
     
-    stage.act(delta);
-    stage.draw();
     game.act(delta);
+    stage.act(delta);
+    
+    spriteBatch.begin();
+    
+    spriteBatch.draw(backgroundTexture, 0, 0);
     game.draw(spriteBatch);
+    
+    spriteBatch.end();
+    
+    stage.draw();
+    
   }
   
 }
